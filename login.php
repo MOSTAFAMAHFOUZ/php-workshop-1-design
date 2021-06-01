e2fbdd349bed27a93c4f8496de1798b9cc36a6d2
MOSTAFAMAHFOUZ <mahfouzm25@gmail.com>
2021-06-01 13:55:33 +0200

    design for php workshop

---
+++ login.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" >
+
+    <title>Login</title>
+  </head>
+  <body>
+    <h1 class="p-5 text-center">Login</h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-6 mx-auto">
+                <form class="p-4 m-3 border bg-gradient-info">
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" class="form-control" id="email" >
+                    </div>
+                    <div class="form-group">
+                        <label for="pass">Password</label>
+                        <input type="password" class="form-control" id="pass" >
+                    </div>
+        
+                    <button type="submit" class="btn btn-success">
+                        <i class="bi bi-reply-all-fill"></i> Submit
+                     </button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" ></script>
+
+  </body>
+</html>
